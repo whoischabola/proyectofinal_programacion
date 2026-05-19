0ecf40dd2377fd14db444d75a1827f2afa4af5ec
whoischabola <ruizcallejonisabel@gmail.com>
2026-05-19 10:37:02 +0200

    Implement all class queries and add documentation

--- src/dao/EventoDAO.java
+++ src/dao/EventoDAO.java
@@ -2,14 +2,13 @@ package dao;
 
 import modelo.Evento;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Map;
 
 public class EventoDAO {
     private String url = "jdbc:sqlite:prueba.sqlite";
 
+    // a.Insertar ---------
     public void insertarEvento(Evento ev) {
         try (Connection conn = DriverManager.getConnection(url)) {
             String sql = "INSERT INTO eventos (nombre, ubicacion, fecha, precio) VALUES (?, ?, ?, ?)";
@@ -23,7 +22,7 @@ public class EventoDAO {
             System.out.println("Error connecting to the database" + e.getMessage());
         }
     }
-
+    // b.Actualizar ---------
     public void actualizarEvento(Evento ev) {
         try (Connection conn = DriverManager.getConnection(url)) {
             String sql = "UPDATE evento SET nombre = ?, ubicacion = ?, precio = ? WHERE id = ? ";
@@ -37,7 +36,7 @@ public class EventoDAO {
             System.out.println("Error connecting to the database" + e.getMessage());
         }
     }
-
+    // c.Borrar---------
     public void borradoEvento(int id) {
         try (Connection conn = DriverManager.getConnection(url)) {
             String sql = "DELETE FROM evento WHERE id = ?";
@@ -48,12 +47,98 @@ public class EventoDAO {
             System.out.println("Error connecting to the database" + e.getMessage());
         }
     }
-    public void obtenerEventosTotalAsistentes() throws SQLException {
+    // d.Obtener número total de asistentes---------
+    public Map<Evento, Integer> obtenerTodosConAsistentes() {
+        String sql = "SELECT e.*, COUNT(i.id_asistente) + e.precio AS total " + "FROM evento e" + "LEFT JOIN inscripcion i ON e.id = i.id_evento " + "GROUP BY e.id";
         try (Connection conn = DriverManager.getConnection(url)) {
-            String sql = "SELECT "
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery(); {
+                while (rs.next()) {
+                    System.out.println(rs.getString("nombre: " + rs.getInt("total") + "asistentes"));
+
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
         }
+        return Map.of();
     }
-}
+    // e. Obtener todos los asistentes segun su ID---------
+    public void obtenerAsistentesPorEvento(int id) throws SQLException {
+        String sql = "SELECT a.* FROM asistente a " + "JOIN inscripcion i ON a.id = i.id_asistente " + "WHERE i.id_evento = ?";
+        try (Connection conn = DriverManager.getConnection(url)) {
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getString("nombre") + " - " + rs.getString("email"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        }
+        // f.Eventos con más de 2 asistentes
+        public void obtenerEventosConMasDe2Asistentes() throws SQLException {
+        String sql = "SELECT e.nombre, COUNT(i.id_asistente) AS total " + "FROM evento e" + "JOIN inscripcion i ON e.id = i.id_evento " + "GROUP BY e.id" + "HAVING COUNT(i.id_asistente) > 2";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery(); {
+        while (rs.next()) {
+            System.out.println(rs.getString("nombre") + " → " + rs.getInt("total") + " asistentes");
+
+        }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        }
+        // g.Top 3 eventos con más ingresos---------
+        public void obtenerTop3Ingresos() {
+            String sql = "SELECT e.nombre, COUNT(i.id_asistente) * e.precio AS ingresos "
+                    + "FROM evento e "
+                    + "LEFT JOIN inscripcion i ON e.id = i.id_evento "
+                    + "GROUP BY e.id "
+                    + "ORDER BY ingresos DESC "
+                    + "LIMIT 3";
+            try (Connection con = DriverManager.getConnection(url);
+                 PreparedStatement ps = con.prepareStatement(sql);
+                 ResultSet rs = ps.executeQuery()) {
+
+                while (rs.next()) {
+                    System.out.println(rs.getString("nombre") + " → " + rs.getDouble("ingresos") + " €");
+                }
+
+            } catch (SQLException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+        // h. Evento más caro de una ubicación---------
+        public void obtenerMasCaroPorUbicacion(String ubicacion) {
+        String sql = "SELECT * FROM evento WHERE ubicacion = ? ORDER BY precio DESC LIMIT 1";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, ubicacion);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                System.out.println(rs.getString("nombre" + rs.getDouble("precio") + " €"));
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        }
+        }
+
+
+
+
+
+
 
 
 
